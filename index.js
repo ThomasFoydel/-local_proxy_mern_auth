0cc041af7cf9fce76143ea3333d33eff4f4e1000
ThomasFoydel <thomasjfoydel@gmail.com>
2020-12-05 16:37:10 -0600

    edit db connection

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const session = require('express-session');
-const dbConnection = require('./database');
+// const dbConnection = require('./database');
 const MongoStore = require('connect-mongo')(session);
 const passport = require('./passport');
 const app = express();
 const PORT = 8000;
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 // Route requires
 const user = require('./routes/user');
 
@@ -21,36 +23,52 @@ app.use(
 app.use(bodyParser.json());
 app.use(cors());
 
-// Sessions
-app.use(
-  session({
-    secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
-    store: new MongoStore({ mongooseConnection: dbConnection }),
-    resave: false, //required
-    saveUninitialized: false, //required
-  })
-);
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/build')); // serve the static react app
-  app.get(/^\/(?!api).*/, (req, res) => {
-    // don't serve api routes to react app
-    res.sendFile(path.join(__dirname, './build/index.html'));
-  });
-  console.log('Serving React App...');
-}
+mongoose
+  .connect(uri)
+  .then(
+    () => {
+      /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+      console.log('Connected to Mongo');
 
-// Passport
-app.use(passport.initialize());
-app.use(passport.session()); // calls the deserializeUser
+      // Sessions
+      app.use(
+        session({
+          secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+          store: new MongoStore({ mongooseConnection: mongoose.connection }),
+          resave: false, //required
+          saveUninitialized: false, //required
+        })
+      );
 
-// Routes
-app.use('/api/user', user);
+      if (process.env.NODE_ENV === 'production') {
+        app.use(express.static('/build')); // serve the static react app
+        app.get(/^\/(?!api).*/, (req, res) => {
+          // don't serve api routes to react app
+          res.sendFile(path.join(__dirname, './build/index.html'));
+        });
+        console.log('Serving React App...');
+      }
 
-// app.post('/api/user', (req, res) => {
-//   console.log('THE ROUTE IS HIT');
-// });
+      // Passport
+      app.use(passport.initialize());
+      app.use(passport.session()); // calls the deserializeUser
 
-// Starting Server
-app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
-});
+      // Routes
+      app.use('/api/user', user);
+
+      // app.post('/api/user', (req, res) => {
+      //   console.log('THE ROUTE IS HIT');
+      // });
+
+      // Starting Server
+      app.listen(PORT, () => {
+        console.log(`App listening on PORT: ${PORT}`);
+      });
+    },
+    (err) => {
+      /** handle initial connection error */
+      console.log('error connecting to Mongo: ');
+      console.log(err);
+    }
+  )
+  .catch((err) => console.log({ err }));
